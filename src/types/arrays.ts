34155e56fbcfbc4faf17a6e10baea55e0a370cd1
Seu Nome <victor_felicio@hotmail.com>
2026-02-16 15:48:03 -0300

    Entendo array no typescript

--- src/types/arrays.ts
+++ src/types/arrays.ts
@@ -1,3 +1,11 @@
 const hobbies = ['Cozinhas', 'Jogar futebol', 123, 'filmes'];
 
 const notas: number[] = [10, 9, 7];
+
+console.log(hobbies);
+
+console.log(hobbies[0]);
+
+console.log(typeof hobbies);
+
+const endereco: [string, number] = ['Rua A', 123];
