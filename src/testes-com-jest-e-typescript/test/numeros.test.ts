36b89c02384c5f4ae2cd4a8b1baf5e2ff68e0969
Victor Felicio <victor_felicio@hotmail.com>
2026-03-24 11:36:23 -0300

    Testando números

---
+++ src/testes-com-jest-e-typescript/test/numeros.test.ts
@@ -0,0 +1,34 @@
+describe('Testando números', () => {
+    test('Deve verificar se um valor é maior que outro', () => {
+        const saldo = 1000;
+        const saque = 500;
+
+        expect(saldo).toBeGreaterThan(saque);
+    });
+
+    test('Deve verificar se um valor é maior ou igual', () => {
+        const saldo = 1000;
+        const saque = 1000;
+
+        expect(saldo).toBeGreaterThanOrEqual(saque);
+    });
+
+    test('Deve verificar se um valor é menor', () => {
+        const saldo = 1000;
+        const saque = 500;
+
+        expect(saque).toBeLessThan(saldo);
+    });
+    test('Deve verificar se um valor é menor ou igual', () => {
+        const saldo = 1000;
+        const saque = 1000;
+
+        expect(saque).toBeLessThanOrEqual(saldo);
+    });
+
+    test('Deve verificar se um valor está próximo de outro', () => {
+        const divisao = 3 / 9;
+
+        expect(divisao).toBeCloseTo(0.33, 2);
+    });
+});
